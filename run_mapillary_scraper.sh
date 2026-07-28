220d9f6bc441e2e40df1674b3f94551fb85eb6d5
Ananthu Aniraj <ananthu.aniraj@inria.fr>
2026-07-28 22:00:30 +0200

    prepare for next cycle of scraping

--- run_mapillary_scraper.sh
+++ run_mapillary_scraper.sh
@@ -3,7 +3,7 @@
 # Configuration
 TOTAL_CHUNKS=10000
 SCRIPT_NAME="src.scrapers.mapillary_scraper"
-BASE_DIR="/home/ananthu/DATA/data_ananthu/mapillary_scrape_rand_5"
+BASE_DIR="/home/ananthu/DATA/data_ananthu/mapillary_scrape_rand_6"
 ORDER_FILE="$BASE_DIR/chunk_order.txt"
 
 # Ensure base directory exists
